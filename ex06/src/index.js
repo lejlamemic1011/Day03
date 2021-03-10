143b95ac34b669f2787233b8d7204c2c94cf72e2
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-10 15:21:36 +0100

    Add files via upload

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,13 @@
+function main() {
+console.log ('Hello Wold')!
+   // kada se runa program ce ispisati hello world 
+var x = 9;  
+var y = 16; 
+var z = My name is John !
 
+return x,y,z;
+}
+main ();
+module.exports = main; 
+
+/* ovim se sve zakomentarise i kraj komentara je */ 
