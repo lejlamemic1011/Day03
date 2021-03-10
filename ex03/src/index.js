f22ed0113bd570411086b070c629c3caa576de0b
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-10 14:18:11 +0100

    Add files via upload

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,1 +1,9 @@
+function main() {
+console.log ('Hello Wold')!
+   // kada se runa program ce ispisati hello world 
+var forCat = "Leli";   
+}
+main ();
+module.exports = main; 
 
+/* ovim se sve zakomentarise i kraj komentara je */ 
