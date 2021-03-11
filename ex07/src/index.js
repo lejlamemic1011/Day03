d50bca149c89c7cbb9c52f08ae8b7b5299983c57
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-03-11 10:40:40 +0100

    Add files via upload

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,1 +1,18 @@
+function main() {
+    // Only change code below this line
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000
 
+
+    // Only change code above this line
+    return {
+studlyCapVar,
+properCamelCase,
+titleCaseOver        
+    }
+
+}
+
+console.log(main());
+module.exports = main;
